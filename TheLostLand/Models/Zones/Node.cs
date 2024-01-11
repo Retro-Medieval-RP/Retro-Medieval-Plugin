12f8f485e7f55864f2c79cd9895b4f454caa837b
Jack Morrison <jack@morrison.ec>
2024-01-11 12:17:33 +0000

    Created the two models for zones.

---
+++ TheLostLand/Models/Zones/Node.cs
@@ -0,0 +1,8 @@
+﻿namespace TheLostLand.Models.Zones;
+
+public class Node
+{
+    internal float X { get; set; }
+    internal float Y { get; set; }
+    internal float Z { get; set; }
+}
---
+++ TheLostLand/Models/Zones/Zone.cs
@@ -0,0 +1,7 @@
+﻿namespace TheLostLand.Models.Zones;
+
+public class Zone
+{
+    internal string ZoneName { get; set; }
+    internal List<Node> Nodes { get; set; }
+}
