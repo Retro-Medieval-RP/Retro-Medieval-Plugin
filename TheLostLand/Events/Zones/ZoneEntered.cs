f007add1ec93ce4e2c3b893ad328f660f4f3e13b
Jack Morrison <jack@morrison.ec>
2024-01-11 12:17:51 +0000

    Created the two events for zones.

---
+++ TheLostLand/Events/Zones/ZoneEntered.cs
@@ -0,0 +1,22 @@
+﻿using Steamworks;
+using TheLostLand.Models.Zones;
+
+namespace TheLostLand.Events.Zones
+{
+    internal class ZoneEnteredEventArgs(CSteamID user_id, Zone zone)
+    {
+        internal CSteamID UserId { get; set; } = user_id;
+        internal Zone Zone { get; set; } = zone;
+    }
+
+    internal class ZoneEnteredEventPublisher
+    {
+        public delegate void ZoneEnteredEventHandler(ZoneEnteredEventArgs e);
+
+        public static event ZoneEnteredEventHandler ZoneEnteredEvent;
+
+        internal static void RaiseEvent(CSteamID user_id, Zone zone) => 
+            ZoneEnteredEvent?.Invoke(new ZoneEnteredEventArgs(user_id, zone));
+    }
+}
+
---
+++ TheLostLand/Events/Zones/ZoneLeft.cs
@@ -0,0 +1,22 @@
+﻿using Steamworks;
+using TheLostLand.Models.Zones;
+
+namespace TheLostLand.Events.Zones
+{
+    internal class ZoneLeftEventArgs(CSteamID user_id, Zone zone)
+    {
+        internal CSteamID UserId { get; set; } = user_id;
+        internal Zone Zone { get; set; } = zone;
+    }
+
+    internal class ZoneLeftEventPublisher
+    {
+        public delegate void ZoneLeftEventHandler(ZoneLeftEventArgs e);
+
+        public static event ZoneLeftEventHandler ZoneLeftEvent;
+
+        internal static void RaiseEvent(CSteamID user_id, Zone zone) => 
+            ZoneLeftEvent?.Invoke(new ZoneLeftEventArgs(user_id, zone));
+    }
+}
+
