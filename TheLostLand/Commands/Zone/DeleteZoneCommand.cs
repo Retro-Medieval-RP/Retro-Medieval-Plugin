38e19894c2431092ea81db07e86ea4f0d334dd05
Jack Morrison <jack@morrison.ec>
2024-01-13 18:04:31 +0000

    Added get zone module for all zones commands.

--- TheLostLand/Commands/Zone/DeleteZoneCommand.cs
+++ TheLostLand/Commands/Zone/DeleteZoneCommand.cs
@@ -1,10 +1,17 @@
-﻿namespace TheLostLand.Commands.Zone;
+﻿using Rocket.Core.Logging;
+using TheLostLand.Modules.Zones;
+
+namespace TheLostLand.Commands.Zone;
 
 public class DeleteZoneCommand : IRocketCommand
 {
     public void Execute(IRocketPlayer caller, string[] command)
     {
-        
+        if (!ModuleLoader.Instance.GetModule<ZonesModule>(out var zones))
+        {
+            Logger.LogError("Could not find or get module [ZonesModule]!");
+            return;
+        }
     }
 
     public AllowedCaller AllowedCaller => AllowedCaller.Both;
--- TheLostLand/Commands/Zone/Nodes/AddNodeCommand.cs
+++ TheLostLand/Commands/Zone/Nodes/AddNodeCommand.cs
@@ -1,10 +1,17 @@
-﻿namespace TheLostLand.Commands.Zone.Nodes;
+﻿using Rocket.Core.Logging;
+using TheLostLand.Modules.Zones;
+
+namespace TheLostLand.Commands.Zone.Nodes;
 
 public class AddNodeCommand : IRocketCommand
 {
     public void Execute(IRocketPlayer caller, string[] command)
     {
-        
+        if (!ModuleLoader.Instance.GetModule<ZonesModule>(out var zones))
+        {
+            Logger.LogError("Could not find or get module [ZonesModule]!");
+            return;
+        }
     }
 
     public AllowedCaller AllowedCaller => AllowedCaller.Both;
