cbb71eb2f8187c1e20deadb3baf044b0c322cfef
Jack Morrison <jack@morrison.ec>
2024-01-14 16:42:53 +0000

    Created the storage class.

--- TheLostLand.Core/Modules/Storage/Storage.cs
+++ TheLostLand.Core/Modules/Storage/Storage.cs
@@ -1,6 +1,7 @@
 ﻿namespace TheLostLand.Core.Modules.Storage;
 
-public class Storage
+public class Storage(string name, IStorage storage)
 {
-    
+    public string StorageName { get; set; } = name;
+    public IStorage Store { get; set; } = storage;
 }
