664c68198f7976a2da21ec099d3bb8108278ad16
Jack Morrison <jack@morrison.ec>
2024-01-18 08:34:55 +0000

    Filled in template for AddNode and RemoveNode commands.

---
+++ TheLostLand/Commands/AddNodeCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Rocket.API;
+
+namespace TheLostLand.Commands;
+
+internal class AddNodeCommand : IRocketCommand
+{
+    public void Execute(IRocketPlayer caller, string[] command)
+    {
+        
+    }
+
+    public AllowedCaller AllowedCaller => AllowedCaller.Player;
+    public string Name => "addnode";
+    public string Help => "Adds a node to a zone.";
+    public string Syntax => "addnode <zone name>";
+    public List<string> Aliases => [];
+    public List<string> Permissions => [];
+}
---
+++ TheLostLand/Commands/RemoveNodeCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Rocket.API;
+
+namespace TheLostLand.Commands;
+
+internal class RemoveNodeCommand : IRocketCommand
+{
+    public void Execute(IRocketPlayer caller, string[] command)
+    {
+        
+    }
+
+    public AllowedCaller AllowedCaller => AllowedCaller.Player;
+    public string Name => "removenode";
+    public string Help => "Removes a node from a zone.";
+    public string Syntax => "removenode <zone name> <id>";
+    public List<string> Aliases => [];
+    public List<string> Permissions => [];
+}
