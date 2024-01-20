f815bcc5f9c961872c9c02abd31240e135fe64a5
Jack Morrison <jack@morrison.ec>
2024-01-20 18:21:44 +0000

    Removed MySql Saver until after rest of modules are completed and can then focus on that.

--- TheLostLand/Modules/Zones/ZonesStorage.cs
+++ TheLostLand/Modules/Zones/ZonesStorage.cs
@@ -1,9 +1,53 @@
-﻿using TheLostLand.Models.Zones;
-using TheLostLand.Savers.MySql;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TheLostLand.Models.Zones;
+using TheLostLand.Savers.Json;
 
 namespace TheLostLand.Modules.Zones;
 
-internal class ZonesStorage : MySqlSaver<Zone>
+public class ZonesStorage : JsonSaver<List<Zone>>
 {
-    
+    public void AddZone(string zone_name)
+    {
+        StorageItem.Add(new Zone(zone_name, []));
+        Save();
+    }
+
+    public void RemoveZone(string zone_name)
+    {
+        StorageItem.RemoveAll(x => x.ZoneName == zone_name);
+        Save();
+    }
+
+    public void AddNode(string zone_name, Node node)
+    {
+        if (StorageItem.All(x => x.ZoneName != zone_name))
+        {
+            return;
+        }
+
+        var zone = StorageItem.Find(x => x.ZoneName == zone_name);
+        zone.Nodes.Add(node);
+
+        Save();
+    }
+
+    public void RemoveNode(string zone_name, int id)
+    {
+        if (StorageItem.All(x => x.ZoneName != zone_name))
+        {
+            return;
+        }
+
+        var zone = StorageItem.Find(x => x.ZoneName == zone_name);
+        zone.Nodes.RemoveAt(id);
+
+        Save();
+    }
+
+    public List<Zone> GetZones() =>
+        StorageItem;
+
+    public Zone GetZone(string zone_name) =>
+        GetZones().Find(x => x.ZoneName == zone_name);
 }
