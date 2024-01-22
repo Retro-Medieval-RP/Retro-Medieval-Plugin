d1ec5b67a4a916661cc03ce641a37149945e076a
Jack Morrison <jack@morrison.ec>
2024-01-22 12:14:44 +0000

    Chore: Added chest id to the chest class for configuration.

--- TheLostLand/Models/LootChest/Chest.cs
+++ TheLostLand/Models/LootChest/Chest.cs
@@ -1,7 +1,8 @@
 ﻿namespace TheLostLand.Models.LootChest;
 
-internal class Chest(double spawn_chance, ChestRarity rarity)
+internal class Chest(double spawn_chance, ChestRarity rarity, ushort chest_id)
 {
     public double ChestSpawnChance { get; set; } = spawn_chance;
     public ChestRarity Rarity { get; set; } = rarity;
+    public ushort ChestBarricade { get; set; } = chest_id;
 }
