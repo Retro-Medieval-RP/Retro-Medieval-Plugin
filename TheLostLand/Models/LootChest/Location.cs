fcebf73ece646cdee7d4b8e01a079bcde458b24d
Jack Morrison <jack@morrison.ec>
2024-01-22 10:44:24 +0000

    Added angle storage for the barricades.

--- TheLostLand/Models/LootChest/Location.cs
+++ TheLostLand/Models/LootChest/Location.cs
@@ -5,4 +5,9 @@ internal class Location
     public float X { get; set; }
     public float Y { get; set; }
     public float Z { get; set; }
+    
+    public float AngleX { get; set; }
+    public float AngleY { get; set; }
+    public float AngleZ { get; set; }
+    public float AngleW { get; set; }
 }
