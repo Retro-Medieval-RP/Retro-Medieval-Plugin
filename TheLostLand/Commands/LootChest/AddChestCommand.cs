4f58b7a8df002c165e9e842a3e85384e598e7f69
Jack Morrison <jack@morrison.ec>
2024-01-22 14:11:05 +0000

    Created the two commands for chests.

---
+++ TheLostLand/Commands/LootChest/AddChestCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Rocket.API;
+
+namespace TheLostLand.Commands.LootChest;
+
+internal class AddChestCommand : IRocketCommand
+{
+    public void Execute(IRocketPlayer caller, string[] command)
+    {
+    }
+
+    public AllowedCaller AllowedCaller => AllowedCaller.Player;
+    public string Name => "addchest";
+    public string Help => "Adds a chest at a location and at an angle.";
+    public string Syntax => "addchest <zone name>";
+    public List<string> Aliases => [];
+    public List<string> Permissions => [];
+}
---
+++ TheLostLand/Commands/LootChest/RemoveChestCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Rocket.API;
+
+namespace TheLostLand.Commands.LootChest;
+
+internal class RemoveChestCommand : IRocketCommand
+{
+    public void Execute(IRocketPlayer caller, string[] command)
+    {
+    }
+
+    public AllowedCaller AllowedCaller => AllowedCaller.Player;
+    public string Name => "removechest";
+    public string Help => "Removes a chest at an ID from a Zone.";
+    public string Syntax => "removechest <zone name> <id>";
+    public List<string> Aliases => [];
+    public List<string> Permissions => [];
+}
