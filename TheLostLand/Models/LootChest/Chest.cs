50e81b30645a2a504868745a90b8b7fed885ec03
Jack Morrison <jack@morrison.ec>
2024-01-22 12:37:02 +0000

    Started adding LootItems so can work on dropping the barricade and filling its storage.

--- TheLostLand/Models/LootChest/Chest.cs
+++ TheLostLand/Models/LootChest/Chest.cs
@@ -1,8 +1,13 @@
-﻿namespace TheLostLand.Models.LootChest;
+﻿using System.Collections.Generic;
+using System.Linq;
 
-internal class Chest(double spawn_chance, ChestRarity rarity, ushort chest_id)
+namespace TheLostLand.Models.LootChest;
+
+internal class Chest(double spawn_chance, ChestRarity rarity, ushort chest_id, params LootItem[] spawn_table)
 {
     public double ChestSpawnChance { get; set; } = spawn_chance;
     public ChestRarity Rarity { get; set; } = rarity;
     public ushort ChestBarricade { get; set; } = chest_id;
+
+    public List<LootItem> SpawnTable { get; set; } = spawn_table.ToList();
 }
---
+++ TheLostLand/Models/LootChest/LootItem.cs
@@ -0,0 +1,7 @@
+﻿namespace TheLostLand.Models.LootChest;
+
+internal class LootItem(ushort item_id, double spawn_chance)
+{
+    public double SpawnChance { get; set; } = spawn_chance;
+    public ushort LootItemID { get; set; } = item_id;
+}
