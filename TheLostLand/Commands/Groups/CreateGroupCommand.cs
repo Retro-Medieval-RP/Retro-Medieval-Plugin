5e73380385cd6e54f780fe77682dfbbb14e5f548
Jack Morrison <jack@morrison.ec>
2024-01-29 15:37:27 +0000

    Filled in the properties for the create group command.

--- TheLostLand/Commands/Groups/CreateGroupCommand.cs
+++ TheLostLand/Commands/Groups/CreateGroupCommand.cs
@@ -7,13 +7,13 @@ public class CreateGroupCommand : IRocketCommand
 {
     public void Execute(IRocketPlayer caller, string[] command)
     {
-        throw new System.NotImplementedException();
+        
     }
 
-    public AllowedCaller AllowedCaller { get; }
-    public string Name { get; }
-    public string Help { get; }
-    public string Syntax { get; }
-    public List<string> Aliases { get; }
-    public List<string> Permissions { get; }
+    public AllowedCaller AllowedCaller => AllowedCaller.Player;
+    public string Name => "creategroup";
+    public string Help => "Creates a group.";
+    public string Syntax => "creategroup <group name>";
+    public List<string> Aliases => [];
+    public List<string> Permissions => [];
 }
