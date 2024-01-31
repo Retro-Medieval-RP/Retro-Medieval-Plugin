e85f00c265dcf09b4626af947e067c2b12cb85ee
Jack Morrison <jack@morrison.ec>
2024-01-31 10:36:19 +0000

    Added code for the different group commands.

--- TheLostLand/Commands/Groups/GroupCommand.cs
+++ TheLostLand/Commands/Groups/GroupCommand.cs
@@ -47,7 +47,7 @@ public class GroupCommand : IRocketCommand
         }
     }
 
-    private void UnbanFromGroup(IRocketPlayer caller, IReadOnlyCollection<string> command)
+    private void UnbanFromGroup(IRocketPlayer caller, IReadOnlyList<string> command)
     {
         if (command.Count < 1)
         {
@@ -55,9 +55,30 @@ public class GroupCommand : IRocketCommand
             UnturnedChat.Say(caller, UnbanFromGroupSyntax, Color.red);
             return;
         }
+        
+        if (!ModuleLoader.Instance.GetModule<GroupsModule>(out var groups_module))
+        {
+            Logger.LogError("Could not find module [GroupsModule]!");
+            return;   
+        }  
+        
+        if (!groups_module.HasPermission((UnturnedPlayer)caller, PermissionLevel.Unban))
+        {
+            UnturnedChat.Say(caller, "You do not have permission to unban users from this group!", Color.red);
+            return;
+        }
+
+        if (groups_module.Unban(command[0]))
+        {
+            UnturnedChat.Say(caller, "You have unbanned user " + command[0] + " from group.");
+            return;
+        }
+
+        UnturnedChat.Say(caller, "Could not unban user: " + command[0]);
+
     }
 
-    private void BanFromGroup(IRocketPlayer caller, IReadOnlyCollection<string> command)
+    private void BanFromGroup(IRocketPlayer caller, IReadOnlyList<string> command)
     {
         if (command.Count < 1)
         {
@@ -65,9 +86,30 @@ public class GroupCommand : IRocketCommand
             UnturnedChat.Say(caller, BanFromGroupSyntax, Color.red);
             return;
         }
+        
+        if (!ModuleLoader.Instance.GetModule<GroupsModule>(out var groups_module))
+        {
+            Logger.LogError("Could not find module [GroupsModule]!");
+            return;   
+        }
+        
+        if (!groups_module.HasPermission((UnturnedPlayer)caller, PermissionLevel.Ban))
+        {
+            UnturnedChat.Say(caller, "You do not have permission to kick users from this group!", Color.red);
+            return;
+        }
+
+        if (groups_module.Ban(command[0]))
+        {
+            UnturnedChat.Say(caller, "You have banned user " + command[0] + " from group.");
+            return;
+        }
+
+        UnturnedChat.Say(caller, "Could not ban user: " + command[0]);
+
     }
 
-    private void KickFromGroup(IRocketPlayer caller, IReadOnlyCollection<string> command)
+    private void KickFromGroup(IRocketPlayer caller, IReadOnlyList<string> command)
     {
         if (command.Count < 1)
         {
@@ -75,13 +117,46 @@ public class GroupCommand : IRocketCommand
             UnturnedChat.Say(caller, KickFromGroupSyntax, Color.red);
             return;
         }
+        
+        if (!ModuleLoader.Instance.GetModule<GroupsModule>(out var groups_module))
+        {
+            Logger.LogError("Could not find module [GroupsModule]!");
+            return;   
+        }
+
+        if (!groups_module.HasPermission((UnturnedPlayer)caller, PermissionLevel.Kick))
+        {
+            UnturnedChat.Say(caller, "You do not have permission to kick users from this group!", Color.red);
+            return;
+        }
+
+        if (groups_module.Kick(command[0]))
+        {
+            UnturnedChat.Say(caller, "You have kicked user " + command[0] + " from group.");
+            return;
+        }
+
+        UnturnedChat.Say(caller, "Could not kick user: " + command[0]);
     }
 
-    private void LeaveGroup(IRocketPlayer caller, IReadOnlyCollection<string> command)
+    private void LeaveGroup(IRocketPlayer caller, IReadOnlyList<string> command)
     {
+        if (!ModuleLoader.Instance.GetModule<GroupsModule>(out var groups_module))
+        {
+            Logger.LogError("Could not find module [GroupsModule]!");
+            return;   
+        }
+
+        if (groups_module.LeaveGroup((UnturnedPlayer)caller))
+        {
+            UnturnedChat.Say(caller, "You have left group: " + command[0]);
+            return;
+        }
+        
+        UnturnedChat.Say(caller, "Could not leave group: " + command[0]);
     }
 
-    private void JoinGroup(IRocketPlayer caller, IReadOnlyCollection<string> command)
+    private void JoinGroup(IRocketPlayer caller, IReadOnlyList<string> command)
     {
         if (command.Count < 1)
         {
@@ -89,9 +164,30 @@ public class GroupCommand : IRocketCommand
             UnturnedChat.Say(caller, JoinGroupSyntax, Color.red);
             return;
         }
+        
+        if (!ModuleLoader.Instance.GetModule<GroupsModule>(out var groups_module))
+        {
+            Logger.LogError("Could not find module [GroupsModule]!");
+            return;   
+        }
+        
+        if (!groups_module.Exists(command[0]))
+        {
+            UnturnedChat.Say(caller, "Error: ", Color.red);
+            UnturnedChat.Say(caller, "A group with the name " + command[0] + " does not exist.", Color.red);
+            return;
+        }
+
+        if (groups_module.JoinGroup((UnturnedPlayer)caller, command[0]))
+        {
+            UnturnedChat.Say(caller, "You have joined group " + command[0]);
+            return;
+        }
+        
+        UnturnedChat.Say(caller, "Could not join group: " + command[0]);
     }
 
-    private void DeleteGroup(IRocketPlayer caller, IReadOnlyCollection<string> command)
+    private void DeleteGroup(IRocketPlayer caller, IReadOnlyList<string> command)
     {
         if (command.Count < 1)
         {
@@ -99,6 +195,28 @@ public class GroupCommand : IRocketCommand
             UnturnedChat.Say(caller, DeleteGroupSyntax, Color.red);
             return;
         }
+        
+        if (!ModuleLoader.Instance.GetModule<GroupsModule>(out var groups_module))
+        {
+            Logger.LogError("Could not find module [GroupsModule]!");
+            return;   
+        }
+
+        if (!groups_module.Exists(command[0]))
+        {
+            UnturnedChat.Say(caller, "Error: ", Color.red);
+            UnturnedChat.Say(caller, "A group with the name " + command[0] + " does not exist.", Color.red);
+            return;
+        }
+
+        if (!groups_module.AnyMembers(command[0]))
+        {
+            groups_module.DeleteGroup(command[0]);
+            UnturnedChat.Say(caller, "Deleted group with name: " + command[0]);
+            return;
+        }
+        
+        UnturnedChat.Say(caller, "The group with name " + command[0] + " still have members.", Color.red);
     }
 
     private void CreateGroup(IRocketPlayer caller, IReadOnlyList<string> command)
