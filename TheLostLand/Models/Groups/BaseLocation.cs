dc5d0e6c22aea65aeafad59a7dc244df3ce08dac
Jack Morrison <jack@morrison.ec>
2024-01-31 16:57:42 +0000

    Fuck it time for groups rework.

--- TheLostLand/Models/Groups/BaseLocation.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TheLostLand.Models.Groups;
-
-public class BaseLocation
-{
-    public float X { get; set; }
-    public float Y { get; set; }
-    public float Z { get; set; }
-}
--- TheLostLand/Models/Groups/GroupMember.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TheLostLand.Models.Groups;
-
-public class GroupMember
-{
-    public ulong UserID { get; set; }
-    public int RankID { get; set; }
-}
