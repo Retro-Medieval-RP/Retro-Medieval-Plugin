6b90615ca420d633c4077cf0ffd87c7cf8f189ae
Jack Morrison <jack@morrison.ec>
2024-02-01 19:02:40 +0000

    Fixed patches

--- TheLostLand/Patches/DamagePatch.cs
+++ TheLostLand/Patches/DamagePatch.cs
@@ -1,25 +1,22 @@
 ﻿using HarmonyLib;
-using Rocket.Unturned.Player;
 using SDG.Unturned;
 using Steamworks;
 using TheLostLand.Events.Unturned;
-using TheLostLand.Modules;
-using TheLostLand.Modules.Death;
 using UnityEngine;
 
-namespace TheLostLand.Patches.Death;
+namespace TheLostLand.Patches;
 
 [HarmonyPatch(typeof(PlayerLife))]
 [HarmonyPatch("doDamage")]
 internal class DamagePatch
 {
-    public static bool Prefix(byte amount, Vector3 new_ragdoll, EDeathCause new_cause, ELimb new_limb, CSteamID new_killer, ref EPlayerKill kill, bool track_kill, ERagdollEffect new_ragdoll_effect, bool can_cause_bleeding, PlayerLife instance)
+    public static bool Prefix(byte amount, Vector3 newRagdoll, EDeathCause newCause, ELimb newLimb, CSteamID newKiller, ref EPlayerKill kill, bool trackKill, ERagdollEffect newRagdollEffect, bool canCauseBleeding, PlayerLife __instance)
     {
-        var ply = instance.channel.owner.player;
+        var ply = __instance.channel.owner.player;
         if (ply is null) return false;
 
         var allow = true;
-        DamageEventEventPublisher.RaiseEvent(amount, new_ragdoll, new_cause, new_limb, new_killer, track_kill, new_ragdoll_effect, can_cause_bleeding, ply, ref kill, ref allow);
+        DamageEventEventPublisher.RaiseEvent(amount, newRagdoll, newCause, newLimb, newKiller, trackKill, newRagdollEffect, canCauseBleeding, ply, ref kill, ref allow);
         return allow;
     }
 }
--- TheLostLand/Patches/GesturePatch.cs
+++ TheLostLand/Patches/GesturePatch.cs
@@ -1,79 +1,23 @@
 ﻿using HarmonyLib;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
-using Steamworks;
 using TheLostLand.Events.Unturned;
 
-namespace TheLostLand.Patches.Death;
+namespace TheLostLand.Patches;
 
 [HarmonyPatch(typeof(PlayerAnimator))]
-[HarmonyPatch("tellGesture")]
+[HarmonyPatch("ReceiveGesture")]
 internal class GesturePatch
 {
-    /// <summary>
-    /// id | Gesture 
-    /// 1  | Inventory Start
-    /// 2  | Inventory Stop
-    /// 3  | Pickup
-    /// 4  | Punch Left
-    /// 5  | Punch Right
-    /// 6  | Surrender Start
-    /// 7  | Surrender Stop
-    /// 13 | Rest Start
-    /// 14 | Rest Stop
-    /// 11 | Arrest Start
-    /// 12 | Arrest Stop
-    /// 8  | Point
-    /// 10 | Salute
-    /// 15 | Facepalm 
-    /// </summary>
-    /// <param name="steamID"></param>
-    /// <param name="id"></param>
-    /// <returns></returns>
-    public static bool Prefix(CSteamID steam_id, byte id)
+    public static bool Prefix(EPlayerGesture newGesture, PlayerAnimator __instance)
     {
-        var allow = true;
-        var player = UnturnedPlayer.FromCSteamID(steam_id);
-        var gesture = EPlayerGesture.NONE;
-
-        var player_current_gesture = player.Player.animator.gesture;
+        var ply = __instance.player;
+        if (ply is null) return false;
         
-        switch (id)
-        {
-            case 1 when player_current_gesture == EPlayerGesture.NONE:
-                gesture = EPlayerGesture.INVENTORY_START;
-                break;
-            case 2 when player_current_gesture == EPlayerGesture.INVENTORY_START:
-            case 3:
-            case 4:
-            case 5:
-                gesture = EPlayerGesture.NONE;
-                break;
-            case 6 when player_current_gesture == EPlayerGesture.NONE:
-                gesture = EPlayerGesture.SURRENDER_START;
-                break;
-            case 7 when player_current_gesture == EPlayerGesture.SURRENDER_START:
-                gesture = EPlayerGesture.NONE;
-                break;
-            case 13 when player_current_gesture == EPlayerGesture.NONE:
-                gesture = EPlayerGesture.REST_START;
-                break;
-            case 14 when player_current_gesture == EPlayerGesture.REST_START:
-                gesture = EPlayerGesture.NONE;
-                break;
-            case 11:
-                gesture = EPlayerGesture.ARREST_START;
-                break;
-            case 12 when player_current_gesture == EPlayerGesture.ARREST_START:
-            case 8 when player_current_gesture == EPlayerGesture.NONE:
-            case 9 when player_current_gesture == EPlayerGesture.NONE:
-            case 10 when player_current_gesture == EPlayerGesture.NONE:
-            case 15 when player_current_gesture == EPlayerGesture.NONE:
-                gesture = EPlayerGesture.NONE;
-                break;
-        }
+        var allow = true;
+        var player = UnturnedPlayer.FromPlayer(ply);
 
-        GestureEventEventPublisher.RaiseEvent(gesture, player, ref allow);
+        GestureEventEventPublisher.RaiseEvent(newGesture, player, ref allow);
         return allow;
     }
 }
