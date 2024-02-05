285c4e37763827e743b57f7f5ed7a7b55f7072b1
Jack Morrison <jack@morrison.ec>
2024-02-05 20:59:54 +0000

    Working on when dequip of one of the set items it dequips all of the items and removes the item.

--- RetroMedieval/Modules/ArmorEquip/ArmorEquipModule.cs
+++ RetroMedieval/Modules/ArmorEquip/ArmorEquipModule.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using RetroMedieval.Events.Unturned.CloathingDequip;
 using RetroMedieval.Events.Unturned.Clothing;
+using RetroMedieval.Models.ArmorEquip;
 using RetroMedieval.Modules.Attributes;
 using Rocket.Core.Logging;
 using Rocket.Unturned.Player;
@@ -15,10 +17,86 @@ namespace RetroMedieval.Modules.ArmorEquip;
 internal class ArmorEquipModule : Module
 {
     private List<CSteamID> IgnoredPlayers { get; set; } = [];
+    private Dictionary<CSteamID, ArmorSet> PlayerSets { get; set; } = [];
 
     public override void Load()
     {
         ClothingEquipEventPublisher.ClothingEquipEvent += OnClothingEquipped;
+        ClothingDequipEventPublisher.ClothingDequipEvent += OnClothingDequipped;
+    }
+
+    private void OnClothingDequipped(ClothingDequipEventArgs e)
+    {
+        if (!PlayerSets.ContainsKey(e.Player.CSteamID) || IgnoredPlayers.Contains(e.Player.CSteamID))
+        {
+            return;
+        }
+
+        var set = PlayerSets[e.Player.CSteamID];
+
+        if (!set.Items.Contains(e.DequippedClothingID) && set.MainItem != e.DequippedClothingID)
+        {
+            return;
+        }
+        
+        ItemManager.dropItem(new Item(set.MainItem, true), e.Player.Position, true, true, false);
+        
+        for (byte i = 0; i < PlayerInventory.PAGES; i++)
+        {
+            if (i == PlayerInventory.AREA)
+                continue;
+
+            var count = e.Player.Inventory.getItemCount(i);
+
+            for (byte index = 0; index < count; index++)
+            {
+                var item = e.Player.Inventory.getItem(i, 0);
+                ItemManager.dropItem(new Item(item.item.id, item.item.amount, item.item.quality, item.item.state),
+                    e.Player.Position, true, true, false);
+                e.Player.Inventory.removeItem(i, 0);
+            }
+        }
+        
+        var remove_unequipped = () =>
+        {
+            for (byte i = 0; i < e.Player.Player.inventory.getItemCount(2); i++)
+            {
+                e.Player.Player.inventory.removeItem(2, 0);
+            }
+        };
+        
+        IgnoredPlayers.Add(e.Player.CSteamID);
+        
+        e.Player.Player.clothing.askWearBackpack(0, 0, Array.Empty<byte>(), true);
+        remove_unequipped();
+        e.Player.Player.clothing.askWearGlasses(0, 0, Array.Empty<byte>(), true);
+        remove_unequipped();
+        e.Player.Player.clothing.askWearHat(0, 0, Array.Empty<byte>(), true);
+        remove_unequipped();
+        e.Player.Player.clothing.askWearPants(0, 0, Array.Empty<byte>(), true);
+        remove_unequipped();
+        e.Player.Player.clothing.askWearMask(0, 0, Array.Empty<byte>(), true);
+        remove_unequipped();
+        e.Player.Player.clothing.askWearShirt(0, 0, Array.Empty<byte>(), true);
+        remove_unequipped();
+        e.Player.Player.clothing.askWearVest(0, 0, Array.Empty<byte>(), true);
+        remove_unequipped();
+        
+        for (byte i = 0; i < PlayerInventory.PAGES; i++)
+        {
+            if (i == PlayerInventory.AREA)
+                continue;
+
+            var count = e.Player.Inventory.getItemCount(i);
+
+            for (byte index = 0; index < count; index++)
+            {
+                e.Player.Inventory.removeItem(i, 0);
+            }
+        }
+        
+        IgnoredPlayers.Remove(e.Player.CSteamID);
+        PlayerSets.Remove(e.Player.CSteamID);
     }
 
     private void OnClothingEquipped(ClothingEquipEventArgs e)
@@ -67,11 +145,15 @@ internal class ArmorEquipModule : Module
                 }
             }
         }
-
+        
+        IgnoredPlayers.Add(e.Player.CSteamID);
         foreach (var item in set.Items)
         {
             Equip(item, e.Player);
         }
+        
+        IgnoredPlayers.Remove(e.Player.CSteamID);
+        PlayerSets.Add(e.Player.CSteamID, set);
     }
 
     private static void Equip(ushort item, UnturnedPlayer player) =>
@@ -79,5 +161,7 @@ internal class ArmorEquipModule : Module
 
     public override void Unload()
     {
+        ClothingEquipEventPublisher.ClothingEquipEvent -= OnClothingEquipped;
+        ClothingDequipEventPublisher.ClothingDequipEvent -= OnClothingDequipped;
     }
 }
