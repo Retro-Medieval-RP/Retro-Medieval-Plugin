e0eb372b65143c30da2de498f2850ec4f8063517
Jack Morrison <jack@morrison.ec>
2024-02-14 10:56:53 +0000

    Removed use for storages and just setup for basic usage.

--- RetroMedieval/Modules/Kits/KitItemsStorage.cs
+++
@@ -1,9 +0,0 @@
-using RetroMedieval.Models.Kits;
-using RetroMedieval.Savers.MySql;
-
-namespace RetroMedieval.Modules.Kits;
-
-internal class KitItemsStorage : MySqlSaver<KitItem>
-{
-    
-}
--- RetroMedieval/Modules/Kits/KitsModule.cs
+++ RetroMedieval/Modules/Kits/KitsModule.cs
@@ -1,29 +1,30 @@
-using System;
+using System.Collections.Generic;
 using RetroMedieval.Models.Kits;
 using RetroMedieval.Modules.Attributes;
+using RetroMedieval.Savers.MySql;
 using RetroMedieval.Savers.MySql.StatementsAndQueries;
 using Rocket.Core.Logging;
 
 namespace RetroMedieval.Modules.Kits;
 
 [ModuleInformation("Kits")]
-[ModuleStorage<KitsStorage>("KitsStorage")]
-[ModuleStorage<KitItemsStorage>("KitItemsStorage")]
+[ModuleStorage<MySqlSaver<Kit>>("KitsStorage")]
+[ModuleStorage<MySqlSaver<KitItem>>("KitItemsStorage")]
 internal class KitsModule : Module
 {
     public override void Load()
     {
     }
 
-    public void CreateKit(Kit kit, params KitItem[] kit_items)
+    public void CreateKit(Kit kit, IEnumerable<KitItem> kit_items)
     {
-        if (!GetStorage<KitsStorage>(out var kits_storage))
+        if (!GetStorage<MySqlSaver<Kit>>(out var kits_storage))
         {
             Logger.LogError("Could not gather storage [KitsStorage]");
             return;
         }
         
-        if(!GetStorage<KitItemsStorage>(out var kit_items_storage))
+        if(!GetStorage<MySqlSaver<KitItem>>(out var kit_items_storage))
         {
             Logger.LogError("Could not gather storage [KitItemsStorage]");
             return;
