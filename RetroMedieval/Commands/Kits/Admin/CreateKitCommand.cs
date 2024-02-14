a3af925ad34049ab21c72180bde3655095f286d6
Jack Morrison <jack@morrison.ec>
2024-02-14 10:58:14 +0000

    Made CreateKitCommand.cs and sending into db.

--- RetroMedieval/Commands/Kits/Admin/CreateKitCommand.cs
+++ RetroMedieval/Commands/Kits/Admin/CreateKitCommand.cs
@@ -1,5 +1,14 @@
 using System.Collections.Generic;
+using Pathfinding.Util;
+using RetroMedieval.Models.Kits;
+using RetroMedieval.Modules;
+using RetroMedieval.Modules.Kits;
 using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
 
 namespace RetroMedieval.Commands.Kits.Admin;
 
@@ -7,6 +16,131 @@ internal class CreateKitCommand : IRocketCommand
 {
     public void Execute(IRocketPlayer caller, string[] command)
     {
+        if (!ModuleLoader.Instance.GetModule<KitsModule>(out var kits_module))
+        {
+            Logger.LogError("Could not find module [KitsModule]!");
+            return;   
+        }
+        
+        if (command.Length < 1)
+        {
+            UnturnedChat.Say(caller, "Syntax Error: ", Color.red);
+            UnturnedChat.Say(caller, Syntax, Color.red);
+            return;
+        }
+
+        var kit = new Kit
+        {
+            KitName = command[0],
+            KitID = Guid.NewGuid()
+        };
+
+        if (command.Length >= 2)
+        {
+            if (int.TryParse(command[1], out var cooldown))
+            {
+                kit.Cooldown = cooldown;
+            }
+            else
+            {
+                UnturnedChat.Say(caller, "Parsing Error: ", Color.red);
+                UnturnedChat.Say(caller, "Could not parse cooldown from input.", Color.red);
+                UnturnedChat.Say(caller, "Syntax: " + Syntax, Color.red);
+                return;
+            }
+        }
+
+        if (caller is not UnturnedPlayer player)
+        {
+            return;
+        }
+        
+        var kit_items = new List<KitItem>();
+        
+        for (byte i = 0; i < PlayerInventory.PAGES; i++)
+        {
+            if (i == PlayerInventory.AREA)
+                continue;
+
+            var count = player.Inventory.getItemCount(i);
+
+            for (byte index = 0; index < count; index++)
+            {
+                var item = player.Inventory.getItem(i, 0);
+                kit_items.Add(new KitItem
+                {
+                    IsEquipped = i is 0 or 1,
+                    ItemAmount = item.item.amount,
+                    ItemID = item.item.id,
+                    ItemState = item.item.state,
+                    ItemQuality = item.item.quality,
+                    KitItemID = Guid.NewGuid()
+                });
+            }
+        }
+        
+        if(player.Player.clothing.backpack != 0)
+            kit_items.Add(new KitItem
+            {
+                IsEquipped = true,
+                ItemAmount = 1,
+                ItemID = player.Player.clothing.backpack,
+                ItemState = player.Player.clothing.backpackState,
+                ItemQuality = player.Player.clothing.backpackQuality,
+                KitItemID = Guid.NewGuid()
+            });
+        if(player.Player.clothing.shirt != 0)
+            kit_items.Add(new KitItem
+            {
+                IsEquipped = true,
+                ItemAmount = 1,
+                ItemID = player.Player.clothing.shirt,
+                ItemState = player.Player.clothing.shirtState,
+                ItemQuality = player.Player.clothing.shirtQuality,
+                KitItemID = Guid.NewGuid()
+            });
+        if(player.Player.clothing.pants != 0)
+            kit_items.Add(new KitItem
+            {
+                IsEquipped = true,
+                ItemAmount = 1,
+                ItemID = player.Player.clothing.pants,
+                ItemState = player.Player.clothing.pantsState,
+                ItemQuality = player.Player.clothing.pantsQuality,
+                KitItemID = Guid.NewGuid()
+            });
+        if(player.Player.clothing.glasses != 0)
+            kit_items.Add(new KitItem
+            {
+                IsEquipped = true,
+                ItemAmount = 1,
+                ItemID = player.Player.clothing.glasses,
+                ItemState = player.Player.clothing.glassesState,
+                ItemQuality = player.Player.clothing.glassesQuality,
+                KitItemID = Guid.NewGuid()
+            });
+        if(player.Player.clothing.vest != 0)
+            kit_items.Add(new KitItem
+            {
+                IsEquipped = true,
+                ItemAmount = 1,
+                ItemID = player.Player.clothing.vest,
+                ItemState = player.Player.clothing.vestState,
+                ItemQuality = player.Player.clothing.vestQuality,
+                KitItemID = Guid.NewGuid()
+            });
+        if(player.Player.clothing.hat != 0)
+            kit_items.Add(new KitItem
+            {
+                IsEquipped = true,
+                ItemAmount = 1,
+                ItemID = player.Player.clothing.hat,
+                ItemState = player.Player.clothing.hatState,
+                ItemQuality = player.Player.clothing.hatQuality,
+                KitItemID = Guid.NewGuid()
+            });
+        
+        kits_module?.CreateKit(kit, kit_items);
     }
 
     public AllowedCaller AllowedCaller => AllowedCaller.Player;
