b701d5e50d188c7d9fc02c42f9d1084050db5144
Jack Morrison <jack@morrison.ec>
2024-02-20 20:44:10 +0000

    Created a switcher so that if I want a switch but cannot use the default switch i can use this.

---
+++ RetroMedieval.Utils/Switcher.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RetroMedieval.Utils;
+
+public class Switcher<T, T2>
+{
+    private List<Decision> Cases { get; set; } = [];
+
+    public Switcher<T, T2> Case(Predicate<T> condition, Action<T2> branch)
+    {
+        var @case = new Decision { Condition = condition, Branch = branch };
+
+        Cases.Add(@case);
+        return this;
+    }
+
+    public void Default(Action<T2> branch) =>
+        Case(x => true, branch);
+
+    public void Switch(T to_switch, T2 instance) =>
+        Switch(to_switch, instance, Cases);
+
+    public void Switch(T to_switch, T2 instance, params Decision[] cases) =>
+        Switch(to_switch, instance, cases.ToList());
+
+    public void Switch(T to_switch, T2 instance, IEnumerable<Decision> cases)
+    {
+        foreach (var @case in cases.Where(@case => @case.Condition(to_switch)))
+        {
+            @case.Branch(instance);
+            break;
+        }
+    }
+
+    public class Decision
+    {
+        public Predicate<T> Condition { get; set; }
+        public Action<T2> Branch { get; set; }
+    }
+}
