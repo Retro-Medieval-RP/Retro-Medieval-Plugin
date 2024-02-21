b8543867b8d1b568b70d6b48ef9abf43147e828f
Jack Morrison <jack@morrison.ec>
2024-02-21 21:31:21 +0000

    Forgot to color some error messages red, also checked if the target user has permission for a specific kit.

--- RetroMedieval/Commands/Kits/User/KitCommand.cs
+++ RetroMedieval/Commands/Kits/User/KitCommand.cs
@@ -37,23 +37,34 @@ internal class KitCommand : IRocketCommand
         if (command.Length >= 2)
         {
             kit_name = command[1];
-            target_player = ulong.TryParse(command[0], out var num) ? UnturnedPlayer.FromCSteamID(new CSteamID(num)) : UnturnedPlayer.FromName(command[0]);
+            target_player = ulong.TryParse(command[0], out var num)
+                ? UnturnedPlayer.FromCSteamID(new CSteamID(num))
+                : UnturnedPlayer.FromName(command[0]);
 
             if (target_player == null)
             {
-                UnturnedChat.Say(caller, "Could not find any users specified.");
+                UnturnedChat.Say(caller, "Could not find any users specified.", Color.red);
                 return;
             }
         }
 
         if (!kits_module.DoesKitExist(kit_name))
         {
-            UnturnedChat.Say(caller, "Could not find kit with name: " + kit_name);
+            UnturnedChat.Say(caller, "Could not find kit with name: " + kit_name, Color.red);
             return;
         }
-        
+
+        if (!caller.IsAdmin || caller is not ConsolePlayer || !target_player.HasPermission($"kit.{kit_name}"))
+        {
+            UnturnedChat.Say(caller,
+                $"{(target_player != null && target_player.Equals((UnturnedPlayer)caller) ? "You do " : target_player?.DisplayName + " does ")} not have permission for kit: {kit_name}", Color.red);
+            return;
+        }
+
         kits_module.SpawnKit(target_player, kit_name);
-        UnturnedChat.Say(caller, "Spawned kit " + kit_name + $"{(target_player != null && !target_player.Equals((UnturnedPlayer)caller) ? " for " + target_player.DisplayName : "")}");
+        UnturnedChat.Say(caller,
+            "Spawned kit " + kit_name +
+            $"{(target_player != null && !target_player.Equals((UnturnedPlayer)caller) ? " for " + target_player.DisplayName : "")}");
     }
 
     public AllowedCaller AllowedCaller => AllowedCaller.Both;
