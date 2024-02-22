285c3fe1c5e440ca7e28a6d82979ced37f854d7c
Jack Morrison <jack@morrison.ec>
2024-02-22 14:10:33 +0000

    Made file scoped

--- RetroMedieval/Events/Unturned/CloathingDequip/HatDequipped.cs
+++ RetroMedieval/Events/Unturned/CloathingDequip/HatDequipped.cs
@@ -1,25 +1,23 @@
 using Rocket.Unturned.Player;
 
-namespace RetroMedieval.Events.Unturned.CloathingDequip
+namespace RetroMedieval.Events.Unturned.CloathingDequip;
+
+public class HatDequippedEventArgs
 {
-    public class HatDequippedEventArgs
-    {
-        public UnturnedPlayer Player { get; set; }
-        public ushort DequippedClothingID { get; set; }
-    }
-
-    public static class HatDequippedEventPublisher
-    {
-        public delegate void HatDequippedEventHandler(HatDequippedEventArgs e);
-
-        public static event HatDequippedEventHandler HatDequippedEvent;
-
-        internal static void RaiseEvent(UnturnedPlayer player, ushort id) =>
-            HatDequippedEvent?.Invoke(new HatDequippedEventArgs()
-            {
-                Player = player,
-                DequippedClothingID = id
-            });
-    }
+    public UnturnedPlayer Player { get; set; }
+    public ushort DequippedClothingID { get; set; }
 }
 
+public static class HatDequippedEventPublisher
+{
+    public delegate void HatDequippedEventHandler(HatDequippedEventArgs e);
+
+    public static event HatDequippedEventHandler HatDequippedEvent;
+
+    internal static void RaiseEvent(UnturnedPlayer player, ushort id) =>
+        HatDequippedEvent?.Invoke(new HatDequippedEventArgs()
+        {
+            Player = player,
+            DequippedClothingID = id
+        });
+}
