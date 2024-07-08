3a1c44dd2723ea1010977fb2ff12f35f08393a6c
Jack Morrison <jack@morrison.ec>
2024-07-08 13:26:22 +0100

    Added code to clear the UI after an amount of time

--- Modules/CityZones/CityZonesModule.cs
+++ Modules/CityZones/CityZonesModule.cs
@@ -1,5 +1,10 @@
-﻿using RetroMedieval.Modules;
+﻿using System.Collections;
+using RetroMedieval.Modules;
 using RetroMedieval.Modules.Attributes;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using UnityEngine;
 using Zones.Events;
 
 namespace CityZones
@@ -21,7 +26,44 @@ namespace CityZones
 
         private void OnZoneEnter(ZoneEnterEventArgs e)
         {
-            throw new System.NotImplementedException();
+            if (!GetStorage<CitiesStorage>(out var storage))
+            {
+                return;
+            }
+
+            if (!GetConfiguration<CityZonesConfiguration>(out var config))
+            {
+                return;
+            }
+
+            if (!storage.CityExists(e.Zone.ZoneName))
+            {
+                return;
+            }
+            
+            var city = storage.GetCity(e.Zone.ZoneName);
+            EffectManager.sendUIEffect(config.ID, 15543, e.Player.Player.channel.GetOwnerTransportConnection(), false);
+            EffectManager.sendUIEffectText(15543, e.Player.Player.channel.GetOwnerTransportConnection(), false, "Text", $"<color=#b25151>{city.WelcomeMessage}</color>{(string.IsNullOrEmpty(city.TerritoryMessage) ? "" : $"\n{city.WelcomeMessage}")}");
+            RetroMedieval.Main.Instance.StartCoroutine(ClearCity(e.Player.CSteamID.m_SteamID, 5));
+        }
+
+        private IEnumerator ClearCity(ulong playerId, int clearTime)
+        {
+            yield return new WaitForSeconds(clearTime);
+            
+            if (UnturnedPlayer.FromCSteamID(new CSteamID(playerId)) == null)
+            {
+                yield break;
+            }
+            
+            var player = UnturnedPlayer.FromCSteamID(new CSteamID(playerId));
+                
+            if (!GetConfiguration<CityZonesConfiguration>(out var config))
+            {
+                yield break;
+            }
+            
+            EffectManager.askEffectClearByID(config.ID, player.Player.channel.GetOwnerTransportConnection());
         }
     }
 }
