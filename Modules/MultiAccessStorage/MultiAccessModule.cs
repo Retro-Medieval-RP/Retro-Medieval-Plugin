9dc93aeb96b6c83fd030b5f5bbef9ebaf2f86062
Jack Morrison <jack@morrison.ec>
2024-07-09 17:22:44 +0100

    Created module class for multi chests

---
+++ Modules/MultiAccessStorage/MultiAccessModule.cs
@@ -0,0 +1,16 @@
+﻿using RetroMedieval.Modules;
+using RetroMedieval.Modules.Attributes;
+
+namespace MultiAccessStorage;
+
+[ModuleInformation("Multi User Access Storage")]
+public class MultiAccessModule(string directory) : Module(directory)
+{
+    public override void Load()
+    {
+    }
+
+    public override void Unload()
+    {
+    }
+}
