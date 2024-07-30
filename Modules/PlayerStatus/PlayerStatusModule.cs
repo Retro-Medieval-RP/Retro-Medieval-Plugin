e2ebc3a9155f4e3134fc3ab1c2ecb41e8876316a
Jack Morrison <jack@morrison.ec>
2024-07-30 15:54:42 +0100

    Started working on the PlayerStatusUI (#23)

---
+++ Modules/PlayerStatus/PlayerStatusModule.cs
@@ -0,0 +1,15 @@
+﻿using RetroMedieval.Modules;
+
+namespace PlayerStatus
+{
+    public class PlayerStatusModule(string directory) : Module(directory)
+    {
+        public override void Load()
+        {
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
