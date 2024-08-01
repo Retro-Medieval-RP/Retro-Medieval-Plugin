de650e72a730e6542437809bfab4b9d375677ef0
Jack Morrison <jack@morrison.ec>
2024-08-01 10:21:19 +0100

    Fixed for issue (#24)

--- Modules/AiBots/Bot/BotData.cs
+++ Modules/AiBots/Bot/BotData.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace AiBots.Bot;
@@ -6,5 +7,19 @@ internal class BotData
 {
     public ulong Id { get; set; }
 
-    public Vector3 Position { get; set; }
+    [JsonIgnore]
+    public Vector3 Position
+    {
+        get => new(X, Y, Z);
+        set
+        {
+            X = value.x;
+            Y = value.y;
+            Z = value.z;
+        }
+    }
+
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
 }
